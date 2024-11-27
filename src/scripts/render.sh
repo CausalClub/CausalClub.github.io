623274faf0c07bfbc6faac71956cd6a4cdc00352
Riccardo Massidda <contact@rmassidda.it>
2024-11-27 18:13:38 +0100

    fix: reintroduce upcoming

--- src/scripts/render.sh
+++ src/scripts/render.sh
@@ -4,7 +4,7 @@ set -e
 cd src/
 
 # Render the pug files
-python3 scripts/render.py Seminars.csv
+python3 scripts/render.py -u Seminars.csv
 
 # Pug rendering
 ../node_modules/.bin/pug --doctype html --pretty layout/index.pug --out .
